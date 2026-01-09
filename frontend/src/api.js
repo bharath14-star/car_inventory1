7fc75007232d78b9f34c25d463babe7105f5c9e6
Bheemesh <bheemesh9221@gmail.com>
2026-01-09 16:30:10 +0530

    update file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://car-inventory1-1.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://car-inventory1-kobo.onrender.com//api',
   //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 // Add request interceptor
