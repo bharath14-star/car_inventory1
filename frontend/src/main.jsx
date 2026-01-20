4296066aff4e37137186e12422bf37c514b749b4
unknown <purnasaisrinivasbusam@gmail.com>
2026-01-20 15:24:14 +0530

    hash to browser

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'animate.css'
@@ -8,8 +8,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename="/car_inventory1">
+    <BrowserRouter basename="/car_inventory1/" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
